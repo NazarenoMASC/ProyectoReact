bbca20c7ff554c72f92d216ff39bcc7418e685e5
NazarenoMASC <nma.silvacordero@gmail.com>
2022-07-25 20:15:20 -0300

    proyecto actual

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ function Card(props) {
   return (
     <div className="cards container-cards">
       <img src={props.imagen}></img>
-      <h3>{props.name}</h3>
+      <h3>nombre{props.name}</h3>
       <p>Categoria: {props.categoria}</p>
       <p>Precio: {props.precio}$</p>
       <Link to={`/item/${props.id}`}>Ver mas detalles</Link>
